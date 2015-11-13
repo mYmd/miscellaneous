1fba9780b8d0acc369006f87e7b815a29c7aaead
mYmd <m-yamada@f06.itscom.net>
2015-11-14 08:25:10 +0900

    Update test_mbind.cpp

--- test_mbind.cpp
+++ test_mbind.cpp
@@ -1,45 +1,34 @@
-//sample for mbind header
+//sample of mbind header
 //Copyright (c) 2014 mmYYmmdd
 
 #include "mbind.hpp"
 #include <iostream>
+#include <string>
+#include <tuple>
 
-template <std::size_t N> struct sizeN { };
-template <typename T>     struct size_of          { static const std::size_t value = sizeof(T); };
-template <std::size_t N> struct size_of<sizeN<N>> { static const std::size_t value = N; };
-//-----------------------
-template <typename...>
-struct is_ascendant	{
-	static const bool value = true;
-};
-template <typename first, typename second, typename...tail>
-struct is_ascendant<first, second, tail...>	{
-	static const bool value = (size_of<first>::value <= size_of<second>::value) && is_ascendant<second, tail...>::value;
-};
-template <typename first, typename second>
-struct is_ascendant<first, second>	{
-	static const bool value = size_of<first>::value <= size_of<second>::value;
-};
-//********************************************************
-int main()
+void test_mbind()
 {
 	using mymd::_x_;	using mymd::_xrr_;
     {
-        using m1 = mymd::mbind<is_ascendant, char, _xrr_, int   , _xrr_, _x_>;
-        using m2 = m1::rebind<int , _xrr_, double, _xrr_, _x_>;
-
-        using m3 = decltype(m1{} || m2{});
-        std::cout << m3::apply<int&, sizeN<9>&, sizeN<16354>>::value << std::endl;
-        std::cout << m3::apply<short&, sizeN<9>&, sizeN<16354>>::value << std::endl;
-
-        using m4 = decltype(m1{} && m2{});
-        std::cout << m4::apply<int&, sizeN<9>&, sizeN<16354>>::value << std::endl;
-        std::cout << m4::apply<short&, sizeN<9>&, sizeN<16354>>::value << std::endl;
+        using mymd::_xrcv_;
+        using tpl = mymd::mbind<std::tuple, char, _x_, int, _xrcv_, int>;
+        auto m = tpl::apply<std::string, const char>{};
+        std::get<0>(m) = 'k';
+        std::get<1>(m) = "std::get<1>(m)";
+        std::get<2>(m) = 3432;
+        std::get<3>(m) = 'Z';
+        std::get<4>(m) = 48;
+        std::cout << std::get<0>(m) << std::endl;
+        std::cout << std::get<1>(m) << std::endl;
+        std::cout << std::get<2>(m) << std::endl;
+        std::cout << std::get<3>(m) << std::endl;
+        std::cout << std::get<4>(m) << std::endl;
     }
     {
-		auto m1 = mymd::mbind<std::is_integral, mymd::_xrr_>{} || mymd::mbind<std::is_floating_point, mymd::_xrr_>{};
-		auto m2 = !mymd::mbind<std::is_same, float, mymd::_xrr_>{};
-		auto m3 = mymd::mbind<std::is_same, std::string, mymd::_xrcvr_>{};
+        using mymd::_xrcvr_;
+		auto m1 = mymd::mbind<std::is_integral, _xrr_>{} || mymd::mbind<std::is_floating_point, _xrr_>{};
+		auto m2 = !mymd::mbind<std::is_same, float, _xrr_>{};
+		auto m3 = mymd::mbind<std::is_same, std::string, _xrcvr_>{};
 		using b = decltype(m1 && m2 || m3);
 		std::cout << b::apply<int>::value << std::endl;
 		std::cout << b::apply<int*>::value << std::endl;
